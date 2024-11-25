21e037fdf5ac613b3eb0cf0856bea38f5b84ffce
Matthew Campbell <m.campbell92@gmail.com>
2024-11-25 11:11:05 +0100

    add test components

--- playground/App.tsx
+++ playground/App.tsx
@@ -6,12 +6,26 @@ export default function App() {
   return (
     <div>
       <Button variant="secondary" size="large">
-        Test
+        Button Button Button
       </Button>
-      <Card
-        title="This is my card"
-        content="Hellooooo I am a card. Hellooooo I am a card. Hellooooo I am a card. Hellooooo I am a card. Hellooooo I am a card. Hellooooo I am a card. Hellooooo I am a card. Hellooooo I am a card. "
-      ></Card>
+      <Card variant="primary">
+        <Card.Title>
+          <h2>This is the title</h2>
+        </Card.Title>
+        <Card.Content>
+          <p>
+            {" "}
+            This is the content. This is the content. This is the content. This
+            is the content. This is the content. This is the content. This is
+            the content. This is the content. This is the content. This is the
+            content. This is the content.
+          </p>
+        </Card.Content>
+      </Card>
+      <Card variant="secondary">
+        <Card.Title>This is the title</Card.Title>
+        <Card.Content>This is the content</Card.Content>
+      </Card>
     </div>
   );
 }
