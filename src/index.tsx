33db2c941b3546f71f1287fe273d04de1bc3fcee
Matthew Campbell <m.campbell92@gmail.com>
2024-11-25 11:11:14 +0100

    add exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,13 @@
 import "./styles/global.css";
 
+// components
 export * from "./components/Button";
+export * from "./components/Card";
+export * from "./components/Modal";
+export * from "./components/TextArea";
+export * from "./components/TextInput";
+
+// layouts
+export * from "./layouts/Navbar";
+export * from "./layouts/Sidebar";
+export * from "./layouts/Footer";
