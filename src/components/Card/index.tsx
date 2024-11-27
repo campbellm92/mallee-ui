5255c8bc3583c4700b129276075073b9c4550837
Matthew Campbell <m.campbell92@gmail.com>
2024-11-27 18:25:26 +0100

    make alt a type instead of hardcoding

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -15,18 +15,27 @@ type CardProps = ComponentProps<"div"> & {
 
 export const Card = ({
   variant = "primary",
-
+  image,
   children,
   ...props
 }: CardProps) => {
-  const className = [styles.card, styles[`card-${variant}`]].join(" ");
+  const className = [styles.card, styles[`card-${variant}`]]
+    .filter(Boolean)
+    .join(" ");
 
   return (
     <div {...props} className={className}>
+      {image && <div className={styles.cardImageContainer}>{image}</div>}
       {children}
     </div>
   );
 };
+
+Card.Image = ({ src, alt }: { src: string; alt: string }) => (
+  <div className={styles.cardImageContainer}>
+    <img className={styles.cardImage} src={src} alt={alt} />
+  </div>
+);
 
 Card.Title = ({ children }: { children: React.ReactNode }) => (
   <div className={styles.cardTitle}>{children}</div>
@@ -34,8 +43,4 @@ Card.Title = ({ children }: { children: React.ReactNode }) => (
 
 Card.Content = ({ children }: { children: React.ReactNode }) => (
   <div className={styles.cardContent}>{children}</div>
-);
-
-Card.Image = ({ src, alt }: { src: string; alt: string }) => (
-  <img className={styles.cardImage} src={src} alt={alt} />
 );
