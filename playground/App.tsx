5d9f5c14a128c5d3c7d75b2cf65b6222a3b65497
Matthew Campbell <m.campbell92@gmail.com>
2024-12-20 16:32:48 +0100

    import navbar for testing

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { Button } from "../src/components/Button/index";
 import { Card } from "../src/components/Card/index";
+import { Navbar } from "../src/layouts/Navbar/index";
 
 export default function App() {
+  const navItems = [
+    { label: "About", href: "/about" },
+    { label: "Contact", href: "/contact" },
+  ];
   return (
     <div>
+      <Navbar
+        variant="with-branding-sticky-fade"
+        logo="THIS IS MY BRAND"
+        navItems={navItems}
+      />
       <Button variant="secondary" size="large">
         Button Button Button
       </Button>
@@ -26,6 +36,42 @@ export default function App() {
         <Card.Title>This is the title</Card.Title>
         <Card.Content>This is the content</Card.Content>
       </Card>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
+        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
+        reiciendis minima.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
+        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
+        reiciendis minima.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
+        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
+        reiciendis minima.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
+        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
+        reiciendis minima.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
+        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
+        reiciendis minima.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
+        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
+        reiciendis minima.
+      </p>
     </div>
   );
 }
