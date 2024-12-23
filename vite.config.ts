0308af6f45d06121c50c293f2d9f6ab7156a010f
Matthew Campbell <m.campbell92@gmail.com>
2024-12-23 10:56:49 +0100

    define config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import libInjectCss from "vite-plugin-lib-inject-css";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  root: "playground", // set root to playground for in-app testing
+  plugins: [react(), libInjectCss()],
+  css: {
+    modules: {
+      localsConvention: "camelCase",
+      generateScopedName: "[name]__[local]___[hash:base64:5]",
+    },
+  },
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: "src/index.tsx",
+      name: "mallee-ui",
+      formats: ["es", "cjs"],
+      fileName: (format) => `mallee-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
