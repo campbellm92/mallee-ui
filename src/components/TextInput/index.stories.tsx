7d69beffda3e91dc68eafc56d96956577879b52b
Matthew Campbell <m.campbell92@gmail.com>
2024-12-30 16:10:27 +0100

    intitial story config for text input comp

--- src/components/TextInput/index.stories.tsx
+++ src/components/TextInput/index.stories.tsx
@@ -1,7 +1,47 @@
 import { TextInput } from "./index";
+import { Meta, StoryObj } from "@storybook/react";
 
-export default {
+const meta: Meta<typeof TextInput> = {
   title: "Components/TextInput",
   component: TextInput,
-  excludeStories: /.*Placeholder$/, // Excludes stories with 'Placeholder' in their name
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component:
+          "An input component for text inputs like name, username or email.",
+      },
+    },
+  },
+  argTypes: {
+    variant: {
+      options: ["basic"],
+      control: { type: "select" },
+    },
+    color: {
+      control: { type: "select" },
+      options: ["primary", "secondary", "tertiary", "neutral"],
+    },
+    name: {
+      control: { type: "text" },
+      description: "The name attribute for the input element.",
+    },
+    label: {
+      control: { type: "text" },
+      description: "The label displayed for the input.",
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: "basic",
+    color: "primary",
+    name: "Email",
+  },
 };
