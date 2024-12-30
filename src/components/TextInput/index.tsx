ee47b20b6137bd01074b71a3686d195aeec93eed
Matthew Campbell <m.campbell92@gmail.com>
2024-12-30 16:10:52 +0100

    create text input component

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,22 +1,40 @@
 import { ComponentProps } from "react";
 import styles from "./styles.module.css";
 
-type TextInputProps = ComponentProps<"div"> & {
+type TextInputProps = ComponentProps<"input"> & {
   variant?: "basic";
+  color?: "primary" | "secondary" | "tertiary" | "neutral";
+  label: string;
+  name: string;
+  placeholder: string;
 };
 
 export const TextInput = ({
   variant = "basic",
-
+  color = "primary",
+  label,
+  name,
+  placeholder = name,
   ...props
 }: TextInputProps) => {
   const className = [
-    styles.div,
-    styles[`div-${variant}`],
-    props.className, // allow additional custom classes
+    styles.input,
+    styles[`input-${variant}`],
+    styles[`input-${color}`],
+    props.className,
   ]
-    .filter(Boolean) // remove any undefined or falsy values
+    .filter(Boolean)
     .join(" ");
 
-  return <div {...props} className={className}></div>;
+  return (
+    <div className={styles.inputContainer}>
+      {label && <label htmlFor={name}>{label}</label>}
+      <input
+        className={className}
+        id={name}
+        placeholder={placeholder}
+        {...props}
+      />
+    </div>
+  );
 };
