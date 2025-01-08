1e555caa19937c7b063b9e789cf504389b2f50ef
Matthew Campbell <m.campbell92@gmail.com>
2025-01-08 17:53:38 +0100

    move cards to separate component

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Button } from "../src/components/Button/index";
-import { Card } from "../src/components/Card/index";
 import { Navbar } from "../src/layouts/Navbar/index";
+import Projects from "./Projects";
 
 export default function App() {
   const navItems = [
@@ -15,63 +14,17 @@ export default function App() {
         logo="THIS IS MY BRAND"
         navItems={navItems}
       />
-      <Button variant="secondary" size="large">
-        Button Button Button
-      </Button>
-      <Card variant="primary">
-        <Card.Title>
-          <h2>This is the title</h2>
-        </Card.Title>
-        <Card.Content>
-          <p>
-            {" "}
-            This is the content. This is the content. This is the content. This
-            is the content. This is the content. This is the content. This is
-            the content. This is the content. This is the content. This is the
-            content. This is the content.
-          </p>
-        </Card.Content>
-      </Card>
-      <Card variant="secondary">
-        <Card.Title>This is the title</Card.Title>
-        <Card.Content>This is the content</Card.Content>
-      </Card>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
-        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
-        reiciendis minima.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
-        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
-        reiciendis minima.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
-        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
-        reiciendis minima.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
-        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
-        reiciendis minima.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
-        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
-        reiciendis minima.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
-        in id molestias voluptas corrupti qui fugit deleniti placeat. Quam
-        ratione sapiente ut nisi illo necessitatibus nihil nam accusantium
-        reiciendis minima.
-      </p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          height: "100vh",
+        }}
+      >
+        <Projects />
+      </div>
     </div>
   );
 }
