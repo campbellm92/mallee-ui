ce3a11908097f8e9bd4f38a7e4a0cb1f3d7de6d2
Matthew Campbell <m.campbell92@gmail.com>
2025-01-09 16:33:41 +0100

    add story for text area

--- src/components/TextArea/index.stories.tsx
+++ src/components/TextArea/index.stories.tsx
@@ -1,7 +1,36 @@
 import { TextArea } from "./index";
+import { Meta, StoryObj } from "@storybook/react";
 
-export default {
+const meta: Meta<typeof TextArea> = {
   title: "Components/TextArea",
   component: TextArea,
-  excludeStories: /.*Placeholder$/, // Excludes stories with 'Placeholder' in their name
+  parameters: {
+    layout: "centered",
+    docs: {
+      description: {
+        component: "A text area component for things like contact forms.",
+      },
+    },
+  },
+  argTypes: {
+    variant: {
+      options: ["basic"],
+      control: { type: "select" },
+    },
+    color: {
+      control: { type: "select" },
+      options: ["primary", "secondary", "tertiary", "neutral"],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: "basic",
+    color: "primary",
+  },
 };
